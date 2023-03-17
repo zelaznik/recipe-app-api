d55c9a5fc873d2cc793763b5aee0a7e083056a39
Steve Zelaznik <steve.zelaznik@gnar.dog>
2023-03-17 10:58:44 -0400

    build feature to wait for database to be ready

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,25 @@
+# django command to wait for the database to be available
+from django.core.management.base import BaseCommand
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+import time
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+
+        self.stdout.write('Waiting for database...')
+
+        while True:
+            try:
+                self.check(databases=['default'])
+                break
+            except (Psycopg2Error, OperationalError):
+                pass
+
+            self.stdout.write('Database unavailable, waiting 1 second')
+            time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available'))
